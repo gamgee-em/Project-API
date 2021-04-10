696c0c0d30002ef19a707230fb8782ebeaaf1d6a
Sam Sweigart <samuel.sweigart@gmail.com>
2021-04-10 17:44:16 -0400

    changed http to https

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,7 +3,7 @@ console.log('js connected');
 // ticketmaster API using the city query
 let tmApiRequest = 'https://app.ticketmaster.com/discovery/v2/events.json?city=philadelphia&apikey=0PYM69m0qo3ESz77SMGYGdnR0YZKo3oM';
 // open weather API call using the city query
-let owApiRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=philadelphia,pa,us&limit=2&appid=84d61ff029585a95fbd34cf405a10229';
+let owApiRequest = 'https://api.openweathermap.org/geo/1.0/direct?q=philadelphia,pa,us&limit=2&appid=84d61ff029585a95fbd34cf405a10229';
  
   // TICKET MASTER API CALL
   const getTmData = async () => {
@@ -23,8 +23,8 @@ let owApiRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=philadelphia,
     let owKey = owData[0];
     // name object
     let cityName = owKey.name;
+    
     console.log(cityName);
-  
     return owData;
 }
 
