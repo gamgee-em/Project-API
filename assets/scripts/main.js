9109ee82902248b97a6aa7a4ff90f7cae084a406
Sam Sweigart <samuel.sweigart@gmail.com>
2021-04-13 07:30:15 -0400

    corrected TmApiRequest url query

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -41,7 +41,7 @@ let iconObj = []
         iconObj.push(icon[i]);
       }
     });
-    console.log(iconObj);
+    console.log(owObj.icon)
 
     for (let i = 0; i < 5; i++) {
       let currentHour = parseInt(moment().format('HH')) + i + 1
@@ -87,7 +87,7 @@ function geoLocate() {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
     // use obj literal to concat lat & lon to url query
-    tmApiRequest = `https://upenn-cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events.json?latlon=${lat}${lon}&apikey=0PYM69m0qo3ESz77SMGYGdnR0YZKo3oM`;
+    tmApiRequest = `https://app.ticketmaster.com/discovery/v2/events.json?latlong=${lat}${lon}&apikey=0PYM69m0qo3ESz77SMGYGdnR0YZKo3oM`;
     // open weather API call using the city query
     owApiRequest = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=84d61ff029585a95fbd34cf405a10229&units=imperial`;
 
