b9b276a7d63e3d865286c102bf2a88d66a5fd9cb
Heather Jackson <hmjackson917@gmail.com>
2021-04-13 22:38:43 -0400

    this branch is my working version of the events function. Do not neccessarily have to merge it

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,6 @@
 console.log('js connected');
 
 // ticketmaster API using the city query
-
 let tmApiRequest = 'https://app.ticketmaster.com/discovery/v2/events.json?city=philadelphia&apikey=0PYM69m0qo3ESz77SMGYGdnR0YZKo3oM';
 // open weather API call using the city query
 let owApiRequest = 'https://api.openweathermap.org/geo/1.0/direct?q=philadelphia,pa,us&limit=2&appid=84d61ff029585a95fbd34cf405a10229&units=imperial';
@@ -9,74 +8,74 @@ let owApiRequest = 'https://api.openweathermap.org/geo/1.0/direct?q=philadelphia
 let hourObj = []
 let iconObj = []
 
-  // TICKET MASTER API CALL
-  const getTmData = async () => {
-    const tmResponse = await fetch(tmApiRequest);
-    const tmData = await tmResponse.json();
-    let tmEmbeddedKey = tmData._embedded;
-    // events object
-    let tmEvents = tmEmbeddedKey.events;
-    console.log(tmEvents)
-    return tmData;
+// TICKET MASTER API CALL
+const getTmData = async () => {
+  const tmResponse = await fetch(tmApiRequest);
+  const tmData = await tmResponse.json();
+  let tmEmbeddedKey = tmData._embedded;
+  // events object
+  let tmEvents = tmEmbeddedKey.events;
+  console.log(tmEvents)
+  return tmData;
 }
 
-  // OPEN WEATHER API CALL
-  const getOwData = async () => {
-    const owResponse = await fetch(owApiRequest);
-    const owData = await owResponse.json();
-    
-    let owObj = {
-      hour: owData.hourly,
-      icon: owData.current.weather
-    };
+// OPEN WEATHER API CALL
+const getOwData = async () => {
+  const owResponse = await fetch(owApiRequest);
+  const owData = await owResponse.json();
 
-    owObj.hour.forEach((hour, i) => {
-      if (i < 5) {
-        hourObj.push(hour.temp);
-      }
-    });
+  let owObj = {
+    hour: owData.hourly,
+    icon: owData.current.weather
+  };
 
-    owObj.icon.forEach((icon, i) => {
-      if (i < 5) {
-        iconObj.push(icon[i]);
-      }
-    });
-    console.log(iconObj);
+  owObj.hour.forEach((hour, i) => {
+    if (i < 5) {
+      hourObj.push(hour.temp);
+    }
+  });
 
-    for (let i = 0; i < 5; i++) {
-      let currentHour = parseInt(moment().format('HH')) + i + 1
-      $('#hour' + i).html(currentHour)
+  owObj.icon.forEach((icon, i) => {
+    if (i < 5) {
+      iconObj.push(icon[i]);
+    }
+  });
+  console.log(iconObj);
 
-      if(currentHour < 12) {
-        $('#hour' + i).html(currentHour + 'am')
-      } else if (currentHour > 12) {
-        $('#hour' + i).html(currentHour - 12 + 'pm')
-      } else if (currentHour === 12) {
-        $('#hour' + i).html(currentHour + 'pm')
-      } else if (currentHour === 24) {
-        $('#hour' + i).html(currentHour + 'am')
-      } 
-    };
+  for (let i = 0; i < 5; i++) {
+    let currentHour = parseInt(moment().format('HH')) + i + 1
+    $('#hour' + i).html(currentHour)
 
-    $('#currentTemp').html('Temp: ' + owData.current.temp + '°F')
-    $('#temp0').html('Temp: ' + hourObj[0] + '°F')
-    $('#temp1').html('Temp: ' + hourObj[1] + '°F')
-    $('#temp2').html('Temp: ' + hourObj[2] + '°F')
-    $('#temp3').html('Temp: ' + hourObj[3] + '°F')
-    $('#temp4').html('Temp: ' + hourObj[4] + '°F')
-    
-    $('#currentIcon').attr('src', `https://openweathermap.org/img/wn/${owData.current.weather[0].icon}@2x.png`)
-    $('#icon0').attr('src', `https://openweathermap.org/img/wn/${owData.hourly[0].weather[0].icon}@2x.png`)
-    $('#icon1').attr('src', `https://openweathermap.org/img/wn/${owData.hourly[1].weather[0].icon}@2x.png`)
-    $('#icon2').attr('src', `https://openweathermap.org/img/wn/${owData.hourly[2].weather[0].icon}@2x.png`)
-    $('#icon3').attr('src', `https://openweathermap.org/img/wn/${owData.hourly[3].weather[0].icon}@2x.png`)
-    $('#icon4').attr('src', `https://openweathermap.org/img/wn/${owData.hourly[4].weather[0].icon}@2x.png`)
-    
+    if(currentHour < 12) {
+      $('#hour' + i).html(currentHour + 'am')
+    } else if (currentHour > 12) {
+      $('#hour' + i).html(currentHour - 12 + 'pm')
+    } else if (currentHour === 12) {
+      $('#hour' + i).html(currentHour + 'pm')
+    } else if (currentHour === 24) {
+      $('#hour' + i).html(currentHour + 'am')
+    } 
+  };
 
-    console.log(hourObj)
-    console.log(owData);
-    return owData;
-}
+  $('#currentTemp').html('Temp: ' + owData.current.temp + '°F')
+  $('#temp0').html('Temp: ' + hourObj[0] + '°F')
+  $('#temp1').html('Temp: ' + hourObj[1] + '°F')
+  $('#temp2').html('Temp: ' + hourObj[2] + '°F')
+  $('#temp3').html('Temp: ' + hourObj[3] + '°F')
+  $('#temp4').html('Temp: ' + hourObj[4] + '°F')
+  
+  $('#currentIcon').attr('src', `https://openweathermap.org/img/wn/${owData.current.weather[0].icon}@2x.png`)
+  $('#icon0').attr('src', `https://openweathermap.org/img/wn/${owData.hourly[0].weather[0].icon}@2x.png`)
+  $('#icon1').attr('src', `https://openweathermap.org/img/wn/${owData.hourly[1].weather[0].icon}@2x.png`)
+  $('#icon2').attr('src', `https://openweathermap.org/img/wn/${owData.hourly[2].weather[0].icon}@2x.png`)
+  $('#icon3').attr('src', `https://openweathermap.org/img/wn/${owData.hourly[3].weather[0].icon}@2x.png`)
+  $('#icon4').attr('src', `https://openweathermap.org/img/wn/${owData.hourly[4].weather[0].icon}@2x.png`)
+  
+
+  console.log(hourObj)
+  console.log(owData);
+  return owData;
+};
 
 // call functions to get data from url querys
 
@@ -84,10 +83,24 @@ let iconObj = []
 function geoLocate() {
   // check if browswer supports navigator method
   function gotPosition(position) {
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const latlon = lat.toString() + "," + lon.toString();
+    const dateObj = new Date();
+    const todaysDate = dateObj.toISOString().split('.')[0]+"Z";
+    const tomorrow = new Date(dateObj.setDate(new Date().getDate() + 1));
+    const tomorrowsDate = tomorrow.toISOString().split('.')[0]+"Z";
+
     // use obj literal to concat lat & lon to url query
-    tmApiRequest = `https://app.ticketmaster.com/discovery/v2/events.json?latlon=${lat}${lon}&apikey=0PYM69m0qo3ESz77SMGYGdnR0YZKo3oM`;
+    //Original
+    tmApiRequest = `https://app.ticketmaster.com/discovery/v2/events.json?latlong=${latlon}&radius=200&unit=miles&startDateTime=${todaysDate}&endDateTime=${tomorrowsDate}&apikey=0PYM69m0qo3ESz77SMGYGdnR0YZKo3oM`;
+    // tmApiRequest = `https://app.ticketmaster.com/discovery/v2/events.json?latlong=${latlon}&radius=5&startDateTime=2021-04-13T11:55:00Z&endDateTime=2021-04-14T11:55:00Z&apikey=0PYM69m0qo3ESz77SMGYGdnR0YZKo3oM`;
+    //Atttemped-failed
+    //tmApiRequest = `https://upenn-cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events.json?latlon=${latlon}&radius=15&startDateTime=${todaysDate}&apikey=0PYM69m0qo3ESz77SMGYGdnR0YZKo3oM`;
+    //Attempted-using "city=Philadelphia"
+    //tmApiRequest = `https://upenn-cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events.json?city=philadelphia&radius=15&startLocalDate=2021-04-13&apikey=0PYM69m0qo3ESz77SMGYGdnR0YZKo3oM`;
+    // Sam's call
+    //tmApiRequest = `https://app.ticketmaster.com/discovery/v2/events.json?latlong=${latlon}&radius=200&unit=miles&size=200&apikey=0PYM69m0qo3ESz77SMGYGdnR0YZKo3oM`;
     // open weather API call using the city query
     owApiRequest = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=84d61ff029585a95fbd34cf405a10229&units=imperial`;
 
