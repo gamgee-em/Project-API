139fe74d73fdb0f582c386e5ddeac15401906070
Sam Sweigart <samuel.sweigart@gmail.com>
2021-04-14 18:28:54 -0400

    added forEach to populate event cards

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,8 +9,8 @@ let owApiRequest = 'https://api.openweathermap.org/geo/1.0/direct?q=philadelphia
 let searchBtn = $('.button');
 
 let eventObj = [];
-let hourObj = []
-let iconObj = []
+let hourObj = [];
+let iconObj = [];
 // displays current day to match TM date structure Example(2021-04-12)
 let currentDate = moment().format('YYYY-MM-DD');
   // TICKET MASTER API CALL
@@ -59,7 +59,6 @@ let currentDate = moment().format('YYYY-MM-DD');
         $(`#distance${i}`).html(`Distance to Venue: ${tmEvents[i].distance} miles`)
 
         // adds link to twitter account
-          console.log(i)
          $(`#tweet${i}`).attr({
           src: tmEvents[0]._embedded.attractions[0].externalLinks.twitter[0].url,
           target: '_blank'
@@ -69,13 +68,15 @@ let currentDate = moment().format('YYYY-MM-DD');
         // checked ticketmaster.com for events happening today &
         // returned 6 results from varying sources
         // look into sources
-        if (currentDate == tmEvents[i].dates.start.localDate) {
+        if (currentDate/*  == tmEvents[i].dates.start.localDate */) {
+          eventObj.push(date);
           console.log('loop is being reached');
         }
-        eventObj.push(date);
+        
 
          
       });
+      console.log(eventObj)
 
     });
     //getting url but link seems to be bad?
@@ -111,7 +112,6 @@ let currentDate = moment().format('YYYY-MM-DD');
     for (let i = 0; i < 5; i++) {
       // parse moment.js value into a number for conditional statement.
       let currentHour = parseInt(moment().format('HH')) + i + 1
-      $('#hour' + i).html(currentHour)
 
       if(currentHour < 12) {
         $('#hour' + i).html(currentHour + ':00am')
@@ -126,15 +126,13 @@ let currentDate = moment().format('YYYY-MM-DD');
         $('#hour' + i).html((currentHour - 12) + ':00am')
       }
 
-      $('#currentTemp').html('Temp: ' + owData.current.temp + '°F')
+      $('#current-temp').html('Temp: ' + owData.current.temp + '°F')
       $(`temp${i}`).html('Temp: ' + hourObj[i] + '°F')
       $(`temp${i}`).html('Temp: ' + hourObj[i] + '°F')
       $(`temp${i}`).html('Temp: ' + hourObj[i] + '°F')
       $(`temp${i}`).html('Temp: ' + hourObj[i] + '°F')
       $(`temp${i}`).html('Temp: ' + hourObj[i] + '°F')
     };
-
-
     
     $('#currentIcon').attr('src', `https://openweathermap.org/img/wn/${owData.current.weather[0].icon}@2x.png`)
     $('#icon0').attr('src', `https://openweathermap.org/img/wn/${owData.hourly[0].weather[0].icon}@2x.png`)
@@ -176,7 +174,7 @@ function geoLocate() {
     let giveUp = 1000 * 30;
     // 1hr
 
-    let tooOld = 100 * 60 * 60;
+    let tooOld = 1000 * 10;
 
     let options = {
       // drains users battery faster by providing more calls and increased location accuracy
